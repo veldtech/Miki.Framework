3762ff177913f4df0c82de6c40d5318dc671cde4
Veld <velddev@gmail.com>
2018-07-28 17:22:47 +0200

    Add: botfilter

---
+++ Miki.Framework/Events/Filters/BotFilter.cs
@@ -0,0 +1,16 @@
+﻿using Miki.Discord.Common;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miki.Framework.Events.Filters
+{
+    public class BotFilter : IFilter
+    {
+		public async Task<bool> FilterAsync(IDiscordMessage msg)
+		{
+			return msg.Author.IsBot;
+		}
+	}
+}
