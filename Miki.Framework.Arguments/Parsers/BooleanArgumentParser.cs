7177744b65da044afa68a647dad9a704462f0be8
Veld <velddev@gmail.com>
2019-01-20 15:36:51 +0100

    Update BooleanArgumentParser.cs

--- Miki.Framework.Arguments/Parsers/BooleanArgumentParser.cs
+++ Miki.Framework.Arguments/Parsers/BooleanArgumentParser.cs
@@ -12,11 +12,20 @@ namespace Miki.Framework.Arguments
 		public int Priority => 1;
 
 		public bool CanParse(IArgumentPack pack)
-			=> bool.TryParse(pack.Peek(), out _);
+			=> bool.TryParse(pack.Peek(), out _) || int.TryParse(pack.Peek(), out _);
 
 		public object Parse(IArgumentPack pack)
 		{
-			return bool.Parse(pack.Take());
+            var item = pack.Take();
+            if (bool.TryParse(item, out bool valueBool))
+            {
+                return valueBool;
+            }
+            else if(int.TryParse(item, out int valueInt))
+            {
+                return valueInt > 0;
+            }
+            return null;
 		}
 	}
 }
