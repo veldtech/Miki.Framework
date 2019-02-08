f8aef6919726c397dfec1b3cdeb9841540827c7b
Veld <velddev@gmail.com>
2019-02-08 13:24:39 +0100

    Create CommandRequirementAttribute.cs

---
+++ Miki.Framework/Events/Attributes/CommandRequirementAttribute.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Miki.Framework.Events.Attributes
+{
+    public abstract class CommandRequirementAttribute : Attribute
+    {
+        public abstract Task<bool> CheckAsync(EventContext e);
+    }
+}
