edab2dbfc6ff0a8d36eaceedbd4a3268835887e1
Mike <veldsinkie.mv@gmail.com>
2019-10-08 22:49:51 +0200

    Add unit tests

---
+++ Miki.Framework.Tests/Commands/Filters/UserFilterTests.cs
@@ -0,0 +1,45 @@
+﻿using System.Threading.Tasks;
+using Miki.Discord.Common;
+using Moq;
+using Xunit;
+
+namespace Miki.Framework.Tests.Commands.Filters
+{
+    using Miki.Framework.Commands.Filters;
+
+    public class UserFilterTests
+    {
+        private readonly UserFilter userFilter;
+
+        private const long BannedUser = 0L;
+        private const long ValidUser = 1L;
+
+        public UserFilterTests()
+        {
+            userFilter = new UserFilter();
+            userFilter.Users.Add(0L);
+        }
+
+        [Theory]
+        [InlineData(BannedUser, false)]
+        [InlineData(ValidUser, true)]
+        public async Task ValidUserTest(long userId, bool allowed)
+        {
+            var isAllowed = await userFilter.CheckAsync(NewContext(userId));
+            Assert.Equal(allowed, isAllowed);
+        }
+
+        private IContext NewContext(long val)
+        {
+            var author = new Mock<IDiscordUser>();
+            author.Setup(x => x.Id)
+                .Returns((ulong)val);
+
+            var context = new Mock<IContext>();
+            context.Setup(x => x.GetContext<IDiscordMessage>("framework-message").Author)
+                .Returns(author.Object);
+
+            return context.Object;
+        }
+    }
+}
---
+++ Miki.Framework.Tests/Commands/Localization/LocalizationServiceTests.cs
@@ -0,0 +1,112 @@
+﻿namespace Miki.Framework.Tests.Commands.Localization
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Miki.Framework.Commands.Localization.Models;
+    using Miki.Framework.Commands.Localization.Models.Exceptions;
+    using Miki.Localization;
+    using Miki.Localization.Models;
+    using Miki.Services.Localization;
+    using Xunit;
+
+    public class LocalizationServiceTests : BaseEntityTest<ChannelLanguage>  
+    {
+        private readonly ILocalizationService service;
+        private readonly TestContext<ChannelLanguage> context;
+
+        private const long ValidId = 0L;
+        private const long InvalidId = 1L;
+
+        public LocalizationServiceTests()
+        {
+            context = NewDbContext();
+            context.Column.AddRange(
+                new ChannelLanguage
+                {
+                    EntityId = 0L,
+                    Language = "dut"
+                });
+            context.SaveChanges();
+            
+            service = new LocalizationService(context);
+            service.AddLocale(new Locale("eng", null));
+            service.AddLocale(new Locale("dut", null));
+        }
+
+        [Fact]
+        public async Task GetValidLocaleTest()
+        {
+            Locale locale = await service.GetLocaleAsync(ValidId);
+
+            Assert.NotNull(locale);
+            Assert.Equal("dut", locale.CountryCode);
+        }
+
+        [Fact]
+        public async Task GetDefaultLocaleTest()
+        {
+            Locale locale = await service.GetLocaleAsync(InvalidId);
+
+            Assert.NotNull(locale);
+            Assert.Equal("eng", locale.CountryCode);
+        }
+
+        [Fact]
+        public async Task GetInvalidLocaleTest()
+        {
+            var serviceNoFallback = new LocalizationService(context, null, new LocalizationService.Config
+            {
+                DefaultIso3 = "invalid"
+            });
+
+            await Assert.ThrowsAsync<LocaleNotFoundException>(
+                async () => await serviceNoFallback.GetLocaleAsync(InvalidId))
+                .ConfigureAwait(false);
+        }
+
+        [Fact]
+        public async Task ListLocalesTest()
+        {
+            var locales = new HashSet<string>();
+            await foreach(var i in service.ListLocalesAsync())
+            {
+                locales.Add(i);
+            }
+
+            Assert.Contains("eng", locales);
+            Assert.Contains("dut", locales);
+            Assert.DoesNotContain("swe", locales);
+        }
+
+        [Fact]
+        public async Task SetLocaleTest()
+        {
+            await service.SetLocaleAsync(ValidId, "eng");
+
+            Locale locale = await service.GetLocaleAsync(ValidId);
+
+            Assert.NotNull(locale);
+            Assert.Equal("eng", locale.CountryCode);
+        }
+
+        [Fact]
+        public async Task AddLocaleTest()
+        {
+            var locales = new HashSet<string>();
+            await foreach(var i in service.ListLocalesAsync())
+            {
+                locales.Add(i);
+            }
+            Assert.Equal(2, locales.Count);
+            
+            service.AddLocale(new Locale("swe", null));
+
+            var newLocales = new HashSet<string>();
+            await foreach(var i in service.ListLocalesAsync())
+            {
+                newLocales.Add(i);
+            }
+            Assert.Equal(3, newLocales.Count);
+        }
+    }
+}
