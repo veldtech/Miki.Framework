0d9009764f61fc4eb5c794fae81ebb6ea6a1887e
Mike <veldsinkie.mv@gmail.com>
2019-10-08 22:56:50 +0200

    Clean up references

---
+++ Miki.Framework/UnitOfWork.cs
@@ -0,0 +1,35 @@
+﻿namespace Miki.Bot.Models.Repositories
+{
+    using System.Threading.Tasks;
+    using Framework;
+    using Microsoft.EntityFrameworkCore;
+    using Patterns.Repositories;
+
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DbContext context;
+
+        public UnitOfWork(DbContext context)
+        {
+            this.context = context;
+        }
+
+        /// <inheritdoc />
+        public IAsyncRepository<T> GetRepository<T>()
+            where T : class
+        {
+            return new EntityRepository<T>(context);
+        }
+
+        /// <inheritdoc />
+        public ValueTask CommitAsync()
+        {
+            return new ValueTask(context.SaveChangesAsync());
+        }
+
+        public void Dispose()
+        {
+            context.Dispose();
+        }
+    }
+}
